a47e38c211f8f9b98cfb4b13f91604179e122a6d
HildebrandoLima <hildebrandolima16@gmail.com>
2024-11-21 17:27:38 -0300

    Ajuestes no codigo de criar cliente

--- app/Data/Repositories/Client/Concretes/CreateClientRepository.php
+++ app/Data/Repositories/Client/Concretes/CreateClientRepository.php
@@ -3,7 +3,9 @@
 namespace App\Data\Repositories\Client\Concretes;
 
 use App\Data\Repositories\Client\Interfaces\ICreateClientRepository;
+use App\Domain\Traits\RequestConfigurator;
 use App\Http\Requests\Client\CreateClientRequest;
+use App\Models\City;
 use App\Models\Client;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -11,19 +13,17 @@ use Exception;
 
 class CreateClientRepository implements ICreateClientRepository
 {
+    private City $city;
+
+    use RequestConfigurator;
+
     public function create(CreateClientRequest $request): bool
     {
         try {
             DB::beginTransaction();
-            Client::query()
-            ->create([
-				'name' => $request->name,
-                'cpf' => $request->cpf,
-				'date_birth' => $request->date_birth,
-				'sex' => $request->sex,
-				'address' => $request->address,
-                'city_id' => $request->city_id,
-            ]);
+            $this->setRequest($request);
+            $this->createCity();
+            $this->createClient();
             DB::commit();
             return true;
         } catch (Exception $e) {
@@ -31,5 +31,26 @@ class CreateClientRepository implements ICreateClientRepository
             Log::error('Erro ao realizar cadastro de cliente:', ['exception' => $e->getMessage()]);
 			return false;
         }
+    }
+
+    private function createCity(): void
+    {
+        $this->city = City::query()->create([
+            'state' => $this->request->state,
+            'city_name' => $this->request->city_name,
+        ]);
+    }
+
+    private function createClient(): void
+    {
+        Client::query()
+        ->create([
+            'name' => $this->request->name,
+            'cpf' => $this->request->cpf,
+            'date_birth' => $this->request->date_birth,
+            'sex' => $this->request->sex,
+            'address' => $this->request->address,
+            'city_id' => $this->city->id,
+        ]);
     }
 }
--- app/Http/Requests/Client/CreateClientRequest.php
+++ app/Http/Requests/Client/CreateClientRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests\Client;
 use App\Domain\Traits\ValidationPermission;
 use App\Http\Requests\BaseRequest;
 use App\Support\Enums\SexEnum;
+use App\Support\Enums\StateEnum;
 use App\Support\Utils\Messages\DefaultErrorMessages;
 
 class CreateClientRequest extends BaseRequest
@@ -20,11 +21,12 @@ class CreateClientRequest extends BaseRequest
     {
         return [
             'name' => 'required|string|unique:client,name',
-			'cpf' => 'string|min:14|max:14|unique:client,cpf',
-			'address' => 'required|string',
-			'date_birth' => 'required|date|before:' . now()->format('d-m-Y'),
-			'sex' => 'required|string|in:' . SexEnum::MASCULINO . ',' . SexEnum::FEMININO,
-            'city_id' => 'required|int',
+            'cpf' => 'string|min:14|max:14|unique:client,cpf',
+            'address' => 'required|string',
+            'date_birth' => 'required|date|before:' . now()->format('d-m-Y'),
+            'sex' => 'required|string|in:' . SexEnum::MASCULINO . ',' . SexEnum::FEMININO,
+            'state' => 'required|string|in:' . implode(',', array_map(fn($case) => $case->value, StateEnum::cases())),
+            'city_name' => 'required|string',
         ];
     }
 
@@ -32,26 +34,25 @@ class CreateClientRequest extends BaseRequest
     {
         return [
             'sex.in' => DefaultErrorMessages::NOT_FOUND,
-			'date_birth.before' => 'Data de nascimento não pode ser hoje ou uma data futura.',
-
+            'state.in' => DefaultErrorMessages::NOT_FOUND,
+            'date_birth.before' => 'Data de nascimento não pode ser hoje ou uma data futura.',
             'name.unique' => DefaultErrorMessages::ALREADY_EXISTING,
             'cpf.unique' => DefaultErrorMessages::ALREADY_EXISTING,
-
             'cpf.min' => DefaultErrorMessages::MIN_CHARACTERS,
             'cpf.max' => DefaultErrorMessages::MAX_CHARACTERS,
-
             'name.required' => DefaultErrorMessages::REQUIRED_FIELD,
             'address.required' => DefaultErrorMessages::REQUIRED_FIELD,
             'date_birth.required' => DefaultErrorMessages::REQUIRED_FIELD,
-			'sex.required' => DefaultErrorMessages::REQUIRED_FIELD,
-            'city_id.required' => DefaultErrorMessages::REQUIRED_FIELD,
-
+            'sex.required' => DefaultErrorMessages::REQUIRED_FIELD,
+            'state.required' => DefaultErrorMessages::REQUIRED_FIELD,
+            'city_name.required' => DefaultErrorMessages::REQUIRED_FIELD,
             'name.string' => DefaultErrorMessages::FIELD_MUST_BE_STRINGER,
             'cpf.string' => DefaultErrorMessages::FIELD_MUST_BE_STRINGER,
             'address.string' => DefaultErrorMessages::FIELD_MUST_BE_STRINGER,
             'date_birth.date' => DefaultErrorMessages::INVALID_DATE,
             'sex.string' => DefaultErrorMessages::FIELD_MUST_BE_STRINGER,
-            'city_id.int' => DefaultErrorMessages::FIELD_MUST_BE_INTEGER,
+            'state.string' => DefaultErrorMessages::FIELD_MUST_BE_STRINGER,
+            'city_name.string' => DefaultErrorMessages::FIELD_MUST_BE_STRINGER,
         ];
     }
 }
